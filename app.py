e2fdff42981c81c7dd6af2473eae899e38ab4338
alwaleedmj <alwaleedmj@gmail.com>
2024-01-12 23:57:41 +0300

    Update dependencies and refactor code

--- app.py
+++ app.py
@@ -1,50 +1,66 @@
 from dotenv import load_dotenv
 import streamlit as st
-from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.llms import OpenAI
-from langchain.callbacks import get_openai_callback
-from langchain.document_loaders import DirectoryLoader
+from langchain.chains import RetrievalQA
+from langchain.document_loaders import TextLoader
 from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import DirectoryLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_community.chat_models import ChatOpenAI
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+import textwrap
 
+def wrap_text_preserve_newlines(text, width=110):
+    # Split the input text into lines based on newline characters
+    lines = text.split('\n')
+
+    # Wrap each line individually
+    wrapped_lines = [textwrap.fill(line, width=width) for line in lines]
+
+    # Join the wrapped lines back together using newline characters
+    wrapped_text = '\n'.join(wrapped_lines)
+
+    return wrapped_text
+
+def process_llm_response(llm_response):
+    print(wrap_text_preserve_newlines(llm_response['result']))
+    print('\n\nSources:')
+    for source in llm_response["source_documents"]:
+        print(f"page number {source.metadata['page']}")
 
 
 def loadingTheFiles():
     loader = DirectoryLoader('files/', glob="./*.pdf", loader_cls=PyPDFLoader)
+
     documents = loader.load()
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
-      )
-    chunks = text_splitter.split_text(documents)
-    return chunks
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    texts = text_splitter.split_documents(documents)
+    return texts
+
 
 def main():
     load_dotenv()
     st.set_page_config(page_title="Ask your PDF")
     st.header("Ask your PDF 💬")
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
-    chunks = loadingTheFiles()
-    embeddings = OpenAIEmbeddings()
-    st.write(embeddings)
-    knowledge_base = FAISS.from_texts(chunks, embeddings)
-    docs = knowledge_base.similarity_search(user_question)
-    # show user input
-    user_question = st.text_input("Ask a question about your PDF:")
+    instructor_embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl", 
+                                                      model_kwargs={"device": "cpu"})
+    embedding = instructor_embeddings
+    vectordb = Chroma(persist_directory='db', embedding_function=embedding)
+    vectordb.persist()
+    retriever = vectordb.as_retriever()
+    retriever = vectordb.as_retriever(search_kwargs={"k": 2})
+    qa_chain = RetrievalQA.from_chain_type(llm=llm, 
+                                  chain_type="stuff", 
+                                  retriever=retriever, 
+                                  return_source_documents=True)
     
-    
-#     llm = OpenAI()
-#     chain = load_qa_chain(llm, chain_type="stuff")
-#     with get_openai_callback() as cb:
-#         response = chain.run(input_documents=docs, question=user_question)
-#         print(cb)
-        
-#     st.write(response)
+    query = st.text_input("Ask your question")
+    llm_response = qa_chain(query)
+    st.write(process_llm_response(llm_response))
     
 
 if __name__ == '__main__':
